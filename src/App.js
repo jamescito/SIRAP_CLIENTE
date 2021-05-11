99dc9fe066c4fe920f7bda1722092ce3139960e2
jamescito <jr76407900@gmail.com>
2021-05-11 15:46:55 -0600

    Instalación y configuración de compatibilidad de tailwind para CRA

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <table class="table-fixed">
+  <thead>
+    <tr>
+      <th class="w-1/2 ...">Title</th>
+      <th class="w-1/4 ...">Author</th>
+      <th class="w-1/4 ...">Views</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Intro to CSS</td>
+      <td>Adam</td>
+      <td>858</td>
+    </tr>
+    <tr class="bg-blue-200">
+      <td>A Long and Winding Tour of the History of UI Frameworks and Tools and the Impact on Design</td>
+      <td>Adam</td>
+      <td>112</td>
+    </tr>
+    <tr>
+      <td>Intro to JavaScript</td>
+      <td>Chris</td>
+      <td>1,280</td>
+    </tr>
+  </tbody>
+</table>
     </div>
   );
 }
